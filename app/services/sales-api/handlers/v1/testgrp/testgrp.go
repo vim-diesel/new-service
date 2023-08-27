ea8e553f88698dcf815eacee5f81cd1c95eb8517
Ian Clark <ian.clark29+github@gmail.com>
2023-08-26 22:27:22 -0500

    cors is working

--- app/services/sales-api/handlers/v1/testgrp/testgrp.go
+++ app/services/sales-api/handlers/v1/testgrp/testgrp.go
@@ -2,22 +2,17 @@ package testgrp
 
 import (
 	"context"
-	"errors"
-	"math/rand"
 	"net/http"
 
-	v1 "github.com/vim-diesel/new-service/business/web/v1"
 	"github.com/vim-diesel/new-service/foundation/web"
 )
 
 // Test is our example route.
 func Test(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-
-	if n := rand.Intn(100); n%2 == 0 {
-		return v1.NewRequestError(errors.New("TRUSTED ERROR"), http.StatusBadRequest)
-	}
+	// if n := rand.Intn(100); n%2 == 0 {
+	// 	return v1.NewRequestError(errors.New("TRUSTED ERROR"), http.StatusBadRequest)
+	// }
 
 	// Validate the data
 	// Call into the business layer
@@ -33,8 +28,6 @@ func Test(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 }
 
 func TestingAuth(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
-
-	w.Header().Set("Access-Control-Allow-Origin", "*")
 
 	status := struct {
 		Status  string
