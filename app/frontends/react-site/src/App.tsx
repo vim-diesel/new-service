2864a43cfe10df661177af89c979eb55c001e831
Ian Clark <ian.clark29+github@gmail.com>
2023-09-02 08:42:30 -0500

    added shadcn ui to frontend

--- app/frontends/react-site/src/App.tsx
+++ app/frontends/react-site/src/App.tsx
@@ -1,13 +1,13 @@
-
+import { Button } from "@/components/ui/button"
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <div>
+        <Button>Click me</Button>
+      </div>
     </>
   )
 }
