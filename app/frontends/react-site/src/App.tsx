bcba338cadbb899c78abde7b2b6cd963ea0d73c8
Ian Clark <ian.clark29+github@gmail.com>
2023-09-02 15:11:17 -0500

    update to frontend

--- app/frontends/react-site/src/App.tsx
+++ app/frontends/react-site/src/App.tsx
@@ -1,13 +1,36 @@
-import { Button } from "@/components/ui/button"
 import './App.css'
+import ProfileCard from './components/MyCard'
+
 
 function App() {
-
   return (
     <>
-      <div>
-        <Button>Click me</Button>
-      </div>
+      <div className="flex flex-row ">
+        <div className="flex-basis-1/3 grow">
+          <ProfileCard
+            title="My Profile"
+            description="This is my profile"
+            content="This is my content"
+            footer="This is my footer"
+            image="../public/cheng-feng-j7AMlh2MMHc-unsplash.jpg" />
+        </div>
+        <div className="flex-basis-1/3 grow">
+          <ProfileCard
+            title="My Profile"
+            description="This is my profile"
+            content="This is my content"
+            footer="This is my footer"
+            image="../public/kevin-laminto-7PqRZK6rbaE-unsplash.jpg" />
+        </div>
+        <div className="flex-basis-1/3 grow">
+          <ProfileCard
+            title="My Profile"
+            description="This is my profile"
+            content="This is my content"
+            footer="This is my footer"
+            image="../public/levon-vardanyan-ihUVzI4f5To-unsplash.jpg" />
+        </div>
+      </div >
     </>
   )
 }
