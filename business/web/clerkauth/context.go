2feb74aa404a625012ecb44315890de013a1200e
Ian Clark <ian.clark29+github@gmail.com>
2023-09-13 10:34:05 -0500

    auth context for userIds

--- business/web/clerkauth/context.go
+++ business/web/clerkauth/context.go
@@ -10,6 +10,9 @@ type ctxKey int
 // key is used to store/retrieve a Claims value from a context.Context.
 const claimKey ctxKey = 1
 
+// key is used to store/retrieve a user value from a context.Context.
+const userKey ctxKey = 2
+
 // =============================================================================
 
 // SetClaims stores the claims in the context.
@@ -22,6 +25,20 @@ func GetClaims(ctx context.Context) ClerkClaims {
 	v, ok := ctx.Value(claimKey).(ClerkClaims)
 	if !ok {
 		return ClerkClaims{}
+	}
+	return v
+}
+
+// SetUserID stores the user id from the request in the context.
+func SetUserID(ctx context.Context, userID string) context.Context {
+	return context.WithValue(ctx, userKey, userID)
+}
+
+// GetUserID returns the claims from the context.
+func GetUserID(ctx context.Context) string {
+	v, ok := ctx.Value(userKey).(string)
+	if !ok {
+		return ""
 	}
 	return v
 }
